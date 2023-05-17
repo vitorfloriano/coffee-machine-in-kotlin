d8c3ce6c6f7eeeef2927c5e360ab9e6d9e2c8c29
vitorfloriano <vitorflorianoc@gmail.com>
2023-05-16 22:34:21 -0300

    Just practicing a bit.

---
+++ Topics/Floating-point types/Distance/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    // put your code here
+    val distance = readLine()!!.toDouble()
+    val time = readLine()!!.toDouble()
+    val speed = distance / time
+    println(speed)
+}
---
+++ Topics/Floating-point types/Evaluate an expression/src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    // put your code here
+    val a = readLine()!!.toDouble()
+    val b = readLine()!!.toDouble()
+    val c = readLine()!!.toDouble()
+    val d = readLine()!!.toDouble()
+    println(a * 10.5 + b * 4.4 + (c + d) / 2.2)
+}
