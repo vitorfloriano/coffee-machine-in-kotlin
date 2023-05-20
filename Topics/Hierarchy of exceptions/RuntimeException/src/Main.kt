7c99e8591c4f132f9284ee61bd6175c44e5fd3fe
vitorfloriano <vitorflorianoc@gmail.com>
2023-05-20 17:25:34 -0300

    Just practicing a bit.

---
+++ Topics/Hierarchy of exceptions/RuntimeException/src/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    try {
+    // write your code here, do not touch the lines above
+        throw RuntimeException()
+    // do not touch the lines below    
+    } catch(e: RuntimeException) {
+        println("Well")
+    } catch (e: Exception) {
+        println("Wrong")
+    }
+}
