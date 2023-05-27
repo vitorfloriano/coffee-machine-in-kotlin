4504a807aa45afe97231a05bdf6a73363c0c9781
vitorfloriano <vitorflorianoc@gmail.com>
2023-05-27 20:00:18 -0300

    Just practicing a bit.

---
+++ Topics/Enum/Currencies/src/Main.kt
@@ -0,0 +1,11 @@
+enum class Currency(val currency: String){
+    USD("United States Dollar"),
+    EUR("Euro"),
+    GBP("Pound Sterling"),
+    RUB("Russian Ruble"),
+    UAH("Ukrainian Hryvnia"),
+    KZT("Kazakhstani Tenge"),
+    CAD("Canadian Dollar"),
+    JPY("Japanese Yen"),
+    CNY("Chinese Yuan"),
+}
